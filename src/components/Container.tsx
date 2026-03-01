417616bc0edc4da74b7082e23b26c67058c2df2c
Provash-roy <provashray94@gmail.com>
2026-03-01 22:53:04 +0600

    basic setup done

---
+++ src/components/Container.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
+  return (
+    <div
+      className={`max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 ${className || ""}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
